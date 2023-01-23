52a7e0e441ebd50c87e1973778d93dc01d4015d4
srsebbi <csperichon@gmail.com>
2023-01-22 23:29:07 -0300

    Created prisma db handler

--- src/utils/db.server.ts
+++ src/utils/db.server.ts
@@ -1,15 +1,24 @@
+// // import { PrismaClient } from '@prisma/client';
+
+// // let db: PrismaClient;
+
+// // declare global {
+// //   var __db: PrismaClient | undefined;
+// // }
+
+// // if (!global.__db) {
+// //   global.__db = new PrismaClient();
+// // }
+
+// // db = global.__db;
 import { PrismaClient } from '@prisma/client';
 
-let db: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare global {
-  var __db: PrismaClient | undefined;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-if (!global.__db) {
-  global.__db = new PrismaClient();
-}
-
-db = global.__db;
-
-export { db };
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
