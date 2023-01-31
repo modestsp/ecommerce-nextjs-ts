20852eae5037aba280c9ed8a8a956b9beec5a6c4
srsebbi <csperichon@gmail.com>
2023-01-31 17:04:51 -0300

    Added SWR config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import type { ReactElement, ReactNode } from 'react';
+import { SWRConfig } from 'swr';
+import fetchJson from '@/lib/fetchJson';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout?: (page: ReactElement, pageProps: any) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
@@ -13,5 +15,16 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetchJson,
+        onError: (err) => {
+          console.error(err);
+        },
+      }}
+    >
+      {getLayout(<Component {...pageProps} />, pageProps)}
+    </SWRConfig>
+  );
 }
