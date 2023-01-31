ab3d5918b88ba5843f56508cedb68c92173bf1b2
srsebbi <csperichon@gmail.com>
2023-01-31 17:04:40 -0300

    Added current user logged to header layout

--- src/components/ShopHeader.tsx
+++ src/components/ShopHeader.tsx
@@ -1,11 +1,32 @@
+import { sessionOptions } from '@/lib/session';
+import useUser from '@/lib/useUser';
+import { withIronSessionSsr } from 'iron-session/next';
 import Link from 'next/link';
+import Router from 'next/router';
 import styles from '../styles/Shop.module.css';
-const ShopHeader = () => {
+
+const ShopHeader = ({ user }: { user: any }) => {
+  const handleLogout = async () => {
+    const logoutUrl = '/api/auth/logout';
+    await fetch(logoutUrl, {
+      method: 'POST',
+    });
+    Router.push('/shop');
+  };
+  console.log('USER', user);
+  // const { user } = useUser({});
+  console.log('USER', user);
   return (
     <header className={styles.header}>
       <Link href={'/'}>LOGO</Link>
       <p>SEARCH BAR</p>
-      <p>USERNAME OR LOGIN</p>
+      {user ? (
+        <div>
+          <p>{user.name}</p> <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <Link href={'/login'}>LOGIN</Link>
+      )}
     </header>
   );
 };
