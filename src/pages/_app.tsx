812c7511d5560efaf9ca15e0c6537d81a02c8267
srsebbi <csperichon@gmail.com>
2023-02-02 19:40:06 -0300

    Created material ui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import type { NextPage } from 'next';
 import type { ReactElement, ReactNode } from 'react';
 import { SWRConfig } from 'swr';
 import fetchJson from '@/lib/fetchJson';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement, pageProps: any) => ReactNode;
@@ -12,6 +14,19 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: 'rgba(255, 255, 255, 1)',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: 'rgba(0, 0, 0, 1)',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
@@ -24,7 +39,9 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         },
       }}
     >
-      {getLayout(<Component {...pageProps} />, pageProps)}
+      <ThemeProvider theme={theme}>
+        {getLayout(<Component {...pageProps} />, pageProps)}
+      </ThemeProvider>
     </SWRConfig>
   );
 }
