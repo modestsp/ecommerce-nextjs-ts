2527254fbcf964a57ea9e7124be1153e56a993a0
srsebbi <csperichon@gmail.com>
2023-02-02 19:41:18 -0300

    Created route for create a review for a product

---
+++ src/pages/api/review/create.ts
@@ -0,0 +1,33 @@
+import { sessionOptions } from '@/lib/session';
+import { prisma } from '@/utils/db.server';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default withIronSessionApiRoute(createReviewRoute, sessionOptions);
+
+async function createReviewRoute(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    console.log('REQ BODY', req.body);
+    console.log('REQ SESSION', req.session);
+
+    const { productId, description } = await JSON.parse(req.body);
+    console.log('PRODUCTID', productId);
+    console.log('DESCRIPTION', description);
+    if (req.session.user) {
+      const { id } = req.session.user;
+      const review = await prisma.review.create({
+        data: {
+          userId: id,
+          productId,
+          description,
+        },
+      });
+      return res.status(201).json({ review });
+    } else {
+      return res.status(405).json({ error: 'User is required' });
+    }
+  } catch (e) {
+    console.log('ERROR', e);
+  }
+  // const user = await validatePassword({ username, password });
+}
