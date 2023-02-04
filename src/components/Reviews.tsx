5d311b321df9534cabb2af29ee8e9e2b1c3b4e81
srsebbi <csperichon@gmail.com>
2023-02-04 19:46:45 -0300

    Added stars rating and add a review functionality

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { Rating, TextField } from '@mui/material';
 import { Review } from '@prisma/client';
 import { useState } from 'react';
 import styles from '../styles/Product.module.css';
@@ -11,6 +11,8 @@ const DisplayReviews = ({
   reviews: Review[];
 }) => {
   const [review, setReview] = useState('');
+  const [reviewValue, setReviewValue] = useState<number | null>(0);
+  console.log('REVIEW VALUE', reviewValue, typeof reviewValue);
   const handleSubmit = async () => {
     const createReviewUrl = '/api/review/create';
     const response = await fetch(createReviewUrl, {
@@ -18,25 +20,41 @@ const DisplayReviews = ({
       body: JSON.stringify({
         productId,
         description: review,
+        value: reviewValue,
       }),
     });
-    console.log('CREATED REVIEW', response.json());
+    const createdReview = await response.json();
+    console.log('CREATED REVIEW', createdReview);
   };
   return (
-    <section className={styles.reviews}>
-      <p>Leave a Review!</p>
-      <TextField
-        id="outlined-basic"
-        label="Write a review"
-        variant="outlined"
-        value={review}
-        onChange={(e) => setReview(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <div>
+    <section className={styles.reviewsSection}>
+      <div className={styles.reviewCreate}>
+        <Rating
+          name="half-rating"
+          value={reviewValue}
+          onChange={(event, newValue) => {
+            setReviewValue(newValue);
+          }}
+          size="small"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Write a review"
+          variant="outlined"
+          value={review}
+          multiline
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <button onClick={handleSubmit} className={styles.submitReviewButton}>
+          Submit
+        </button>
+      </div>
+
+      <div className={styles.reviewsContainer}>
+        <h3>Reviews</h3>
         {reviews.map((review) => {
           return (
-            <article key={review.id}>
+            <article key={review.id} className={styles.reviewCard}>
               <span>
                 <p>{review.value}</p>
                 <p>{String(new Date(review.createdAt))}</p>
