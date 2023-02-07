680725e0e3518f5cac1522c41abe45c75f9b8990
srsebbi <csperichon@gmail.com>
2023-02-07 20:25:09 -0300

    Created services for fetching products: single or multiple

--- src/services/server/product.ts
+++ src/services/server/product.ts
@@ -1,8 +1,33 @@
-const getProducts = async () => {
+import { ProductWithReviews } from '@/pages/shop/[category]/[productId]';
+import { Product } from '@prisma/client';
+import { useRouter } from 'next/router';
+
+interface ProductData {
+  product: ProductWithReviews;
+  relatedProducts: Product[];
+}
+
+const getProducts = async (): Promise<Product[] | ProductWithReviews[]> => {
   const response = await fetch('/api/products');
+  return response.json();
+};
+
+const getProductsFromCategory = async (
+  category: any
+): Promise<Product[] | ProductWithReviews[]> => {
+  const response = await fetch(`/api/products/${category}`);
+  return response.json();
+};
+
+const getSingleProduct = async (query: any): Promise<ProductData> => {
+  const response = await fetch(
+    `/api/products/${query.category}/${query.productId}`
+  );
   return response.json();
 };
 
 export default {
   getProducts,
+  getProductsFromCategory,
+  getSingleProduct,
 };
