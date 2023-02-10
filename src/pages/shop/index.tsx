2e16cf208f7b096d7d16c39ffbe7dbd72178c73b
srsebbi <csperichon@gmail.com>
2023-02-09 21:52:25 -0300

    Added product card to display in product gallery

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -5,6 +5,10 @@ import CategoriesSidebar from '@/components/CatergoriesSidebar';
 import ShopHeader from '@/components/ShopHeader';
 import { UserWithSession } from '@/lib/session';
 import { useGetProducts } from '@/hooks/useGetProducts';
+import imageLoader from '@/utils/imageLoader';
+import Image from 'next/image';
+import { Rating } from '@mui/material';
+import { Router, useRouter } from 'next/router';
 
 type Props = {
   products: string;
@@ -17,18 +21,49 @@ export type ShopPageWithLayout = NextPage<Props> & {
 
 const Shop: ShopPageWithLayout = () => {
   const { isLoading: loadingProducts, data: products } = useGetProducts();
-
+  const router = useRouter();
+  const currentRoute = router.asPath;
   if (loadingProducts) {
     return <div>Loading!</div>;
   }
-
+  console.log(products);
   return (
-    <div className={styles.productsContainer}>
-      <div className={styles.productCard}>
-        {products!.map((product: any) => {
-          return <p key={product.id}>{product.name}</p>;
-        })}
-      </div>
+    <div className={styles.productsGalleryContainer}>
+      {products!.map((product: any) => {
+        return (
+          <article className={styles.productCard} key={product.id}>
+            <div className={styles.productHeader}>
+              <Rating
+                name="read-only"
+                value={product.stars}
+                readOnly
+                size="small"
+                className={styles.productRating}
+              />
+              <p>{product.name}</p>
+            </div>
+            <Image
+              onClick={() =>
+                router.push(
+                  `${currentRoute}/${product.category.name}/${product.id}`
+                )
+              }
+              className={styles.productImage}
+              src={product.url}
+              alt={product.name}
+              width={200}
+              height={200}
+              loader={imageLoader}
+              unoptimized // ver esto  has a "loader" property that does not implement width.
+              // Please implement it or use the "unoptimized" property instead.
+            />
+            <div className={styles.productFooter}>
+              <p>{product.price}$</p>
+              <button className={styles.addToCartButton}>Add to Cart</button>
+            </div>
+          </article>
+        );
+      })}
     </div>
   );
 };
@@ -41,6 +76,7 @@ Shop.getLayout = function getLayout(page: ReactElement) {
         <CategoriesSidebar />
         {page}
       </section>
+      <footer className={styles.footer}>Footer</footer>
     </main>
   );
 };
