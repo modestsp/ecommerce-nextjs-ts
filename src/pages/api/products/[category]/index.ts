3014fed54c0cda66452f6729dd2679e68e732a9c
srsebbi <csperichon@gmail.com>
2023-02-09 21:52:52 -0300

    Added route for fetching products from a category

---
+++ src/pages/api/products/[category]/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@/utils/db.server';
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log('URL IN API', req.url);
+  const category = req.url?.substring(req.url.lastIndexOf('/') + 1);
+  try {
+    if (category) {
+      const products = await prisma.product.findMany({
+        where: {
+          category: {
+            name: category as string,
+          },
+        },
+      });
+      return res.status(200).json(products);
+    }
+  } catch (e) {
+    res.status(404).json({ error: 'No category were provided' });
+  }
+}
