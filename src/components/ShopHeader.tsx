af78be29df264ea875a1bf2ee82b532fde28d97b
srsebbi <csperichon@gmail.com>
2023-02-09 21:53:26 -0300

    Added icons

--- src/components/ShopHeader.tsx
+++ src/components/ShopHeader.tsx
@@ -4,6 +4,9 @@ import { useShopStore } from '@/lib/store';
 import Link from 'next/link';
 import Router from 'next/router';
 import styles from '../styles/Shop.module.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const ShopHeader = () => {
   const { isLoading, data: user } = useGetUser();
@@ -19,13 +22,17 @@ const ShopHeader = () => {
 
   return (
     <header className={styles.header}>
-      <Link href={'/'}>LOGO</Link>
-      <p>SEARCH BAR</p>
+      <Link href={'/'} className={styles.headerLogo}>
+        LOGO
+      </Link>
+      <div className={styles.searchBar} style={{ color: 'white' }}>
+        <SearchIcon />
+      </div>
       {user ? (
         <div className={styles.userData}>
           <p>{user.name}</p>
-          <p>Cart</p>
-          <button onClick={handleLogout}>Logout</button>
+          <ShoppingCartIcon style={{ color: 'white' }} />
+          <LogoutIcon onClick={handleLogout} style={{ color: 'white' }} />
         </div>
       ) : (
         <Link href={'/login'}>LOGIN</Link>
