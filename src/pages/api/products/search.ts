ddeade05db09a07052087bbe5f3e522a23a1680d
srsebbi <csperichon@gmail.com>
2023-02-12 20:55:20 -0300

    Created search api route

---
+++ src/pages/api/products/search.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '@/utils/db.server';
+
+export default async function getAllProductsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log('HELLO WORLD');
+  try {
+    const search = req.query.search;
+    if (search) {
+      const products = await prisma.product.findMany({
+        where: {
+          OR: [
+            {
+              name: {
+                contains: search as string,
+              },
+            },
+            {
+              tags: {
+                hasSome: search,
+              },
+            },
+          ],
+        },
+      });
+      return res.status(200).json(products);
+    }
+  } catch (e) {
+    console.log('ERROR **********', e);
+  }
+}
