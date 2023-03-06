5af7923e4127a2e282558c25037095bce0bef511
srsebbi <csperichon@gmail.com>
2023-03-06 20:02:58 -0300

    Changed animation for cartmodal, and added PreviewCard for product

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -2,6 +2,32 @@ import styles from '../styles/Shop.module.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import { dropIn, fromRightToLeft } from '@/utils/animations';
 import { useShopStore } from '@/lib/store';
+import Image from 'next/image';
+import imageLoader from '@/utils/imageLoader';
+
+export const variants = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 0.4,
+      type: 'ease',
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      type: 'ease',
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.4,
+      type: 'ease',
+    },
+  },
+};
 
 const CartModal = ({
   toggleCart,
@@ -11,13 +37,14 @@ const CartModal = ({
   setToggleCart: any;
 }) => {
   const cart = useShopStore((state) => state.cart);
+  const totalPrice = useShopStore((state) => state.totalPrice);
 
   return (
     <motion.div
       initial="hidden"
       animate="visible"
       exit="exit"
-      variants={fromRightToLeft}
+      variants={variants}
       className={styles.cartModal}
     >
       <button
@@ -28,12 +55,33 @@ const CartModal = ({
       </button>
       <p>CART</p>
       {cart.length > 0 && (
-        <ul>
+        <ul className={styles.cartProducts}>
           {cart.map((product) => {
-            return <li key={product.id}>{product.price}</li>;
+            return (
+              <li className={styles.cartProductCard} key={product.id}>
+                <Image
+                  src={product.url}
+                  width={100}
+                  height={100}
+                  alt={product.name}
+                  loader={imageLoader}
+                />
+                <div className={styles.cartProductDetails}>
+                  <p>{product.name}</p>
+                  <p>Qty:{cart.find((p) => p.id === product.id)?.quantity}</p>
+                  <span>
+                    Price:{' '}
+                    {product.price! *
+                      cart.find((p) => p.id === product.id)?.quantity!}
+                    $
+                  </span>
+                </div>
+              </li>
+            );
           })}
         </ul>
       )}
+      <p>Total: {totalPrice}$</p>
     </motion.div>
   );
 };
