ebb46b7136ae24b1895161c166f24a7316d8e56a
srsebbi <csperichon@gmail.com>
2023-03-15 20:07:44 -0300

    Removed unused types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,17 +1,11 @@
 import { TypeOf } from 'zod';
 import { createSessionSchema } from '@/pages/login';
 import { createUserSchema } from '@/pages/signup';
-import { createTodoSchema } from './components/Home/Main/Todos/CreateTodoForm';
-import { createProjectSchema } from './components/Home/Main/Projects/CreateProjectForm';
-import { updateTodoSchema } from './components/Home/Main/Todos/UpdateTodo';
-import { updateProjectSchema } from './components/Home/Main/Projects/UpdateProjectForm';
+import { createReviewSchema } from '@/components/Reviews';
 
 export type CreateSessionInput = TypeOf<typeof createSessionSchema>;
 export type CreateUserInput = TypeOf<typeof createUserSchema>;
-export type CreateTodoInput = TypeOf<typeof createTodoSchema>;
-export type CreateProjectInput = TypeOf<typeof createProjectSchema>;
-export type UpdateTodoInput = TypeOf<typeof updateTodoSchema>;
-export type UpdateProjectInput = TypeOf<typeof updateProjectSchema>;
+export type CreateReviewInput = TypeOf<typeof createReviewSchema>;
 
 export interface User {
   id: string;
@@ -21,24 +15,4 @@ export interface User {
   exp: number;
   name: string;
   session: string;
-  todos?: Todo[];
-}
-
-export interface Todo {
-  id: string;
-  description: string;
-  createdAt: string;
-  endsAt: string;
-  completed: boolean;
-  creatorId: string;
-  projectId?: string;
-}
-
-export interface Project {
-  id: string;
-  title: string;
-  creatorId: string;
-  createdAt: string;
-  endsAt: string;
-  todos?: Todo[];
 }
